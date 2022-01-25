d8637fb4165bdce22ab0597caf4bd20661dcc4df
RAMA4368 <ksooyeun4368@gmail.com>
2022-01-25 16:52:03 +0900

    update All

---
+++ src/main/java/com/koreait/cobox/model/domain/Admin.java
@@ -0,0 +1,48 @@
+package com.koreait.cobox.model.domain;
+
+public class Admin {
+
+	private int admin_seq;
+	private String admin_id;
+	private String admin_pwd;
+	private String admin_nm;
+	private String admin_role;
+	private String regist_dt;
+	
+	public String getRegist_dt() {
+		return regist_dt;
+	}
+	public void setRegist_dt(String regist_dt) {
+		this.regist_dt = regist_dt;
+	}
+	public int getAdmin_seq() {
+		return admin_seq;
+	}
+	public void setAdmin_seq(int admin_seq) {
+		this.admin_seq = admin_seq;
+	}
+	public String getAdmin_id() {
+		return admin_id;
+	}
+	public void setAdmin_id(String admin_id) {
+		this.admin_id = admin_id;
+	}
+	public String getAdmin_pwd() {
+		return admin_pwd;
+	}
+	public void setAdmin_pwd(String admin_pwd) {
+		this.admin_pwd = admin_pwd;
+	}
+	public String getAdmin_nm() {
+		return admin_nm;
+	}
+	public void setAdmin_nm(String admin_nm) {
+		this.admin_nm = admin_nm;
+	}
+	public String getAdmin_role() {
+		return admin_role;
+	}
+	public void setAdmin_role(String admin_role) {
+		this.admin_role = admin_role;
+	}
+}
